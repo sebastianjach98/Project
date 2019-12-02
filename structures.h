9d3a944f2d7b7fd188f947cff8e8c5eb51274712
Sebastian Jach <49071410+sebastianjach98@users.noreply.github.com>
2019-12-02 17:03:12 -0500

    Add files via upload

---
+++ structures.h
@@ -0,0 +1,9 @@
+// Sebastian Jach
+#pragma once
+
+#include "linkedlist.h"
+#include "doublylinkedlist.h"
+#include "circularlinkedlist.h"
+#include "stack.h"
+#include "queue.h"
+#include "functions.h"
